6f2bd0144ca3ab09e5cb2bf2eaee699f1d80904b
Anthony Ikeri <ikerianthony251@gmail.com>
2025-06-10 18:02:55 +0100

    fix root to serve index.html and deploy correctly

---
+++ index.js
@@ -0,0 +1,21 @@
+git
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve your index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
