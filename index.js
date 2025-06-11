7a70f0e7266148a776abb29877a6cb7931cfbcd1
Anthony Ikeri <ikerianthony251@gmail.com>
2025-06-11 14:30:53 +0100

    Fix: overwrite index.js with working server code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-git
 
 const express = require('express');
 const path = require('path');
@@ -8,12 +7,11 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to serve your index.html
-app.get('/', (req, res) => {
+// Send index.html for any other route
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
