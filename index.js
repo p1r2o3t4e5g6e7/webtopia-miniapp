76fa6ca2d7334bcaf32bece1b8cc694063344211
Anthony Ikeri <ikerianthony251@gmail.com>
2025-06-11 19:29:17 +0100

    Fix: clean and complete index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 
@@ -15,5 +16,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
 
