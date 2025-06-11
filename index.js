75f19d8b3f797a1bfc1178e9ac5d28e068b525fd
Anthony Ikeri <ikerianthony251@gmail.com>
2025-06-11 17:23:34 +0100

    Fix: serve index.html correctly from root route

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" folder
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Send index.html for any other route
-app.get('*', (req, res) => {
+// Serve index.html for root route
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
